fcb292e0dbaaa46eda5dd163399d99d8e9045b95
petejayakant <pjkant@lassons.com>
2025-11-11 15:37:07 -0700

    Remove target folder from Git

--- customer-microservice/src/main/java/com/lassons/customer/controller/CustomerController.java
+++ customer-microservice/src/main/java/com/lassons/customer/controller/CustomerController.java
@@ -4,6 +4,8 @@ import com.lassons.customer.repository.CustomerRepository;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -12,6 +14,11 @@ public class CustomerController {
 
     @Resource
     private CustomerRepository repository;
+
+    @GetMapping("/hello")  // <-- Add this
+    public String hello() {
+        return "Hello from Customer Controller!";
+    }
 
     @GetMapping("/{id}")
     public Customer getCustomer(@PathVariable String id) {
@@ -23,5 +30,13 @@ public class CustomerController {
     public Customer putCustomer(@PathVariable String id, @RequestBody Customer updatedCustomer) {
         updatedCustomer.setId(id);
         return repository.save(updatedCustomer);
+    }
+    @PostMapping("/batch")
+    public List<Customer> createCustomers(@RequestBody List<Customer> customers) {
+        return repository.saveAll(customers);
+    }
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return repository.findAll();
     }
 }
